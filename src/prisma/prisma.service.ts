640259310bb189446d442112874c1c6c84d277e4
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-06-26 01:37:46 +0900

    prismaが動くように修正

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,20 +1,9 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    // Connect to the database
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    // Disconnect from the database
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    await this.$disconnect();
   }
 }
